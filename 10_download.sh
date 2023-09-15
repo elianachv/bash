c94de6ba4703f6d909c73174778a635f82cb47da
Eliana Chavez <elianachavezv@gmail.com>
2023-09-15 20:02:25 +0000

    [DevOps][Class-15][Feature] Download some stuff from internet

---
+++ 10_download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Script to download info from internet
+# Author Eliana Chavez
+
+echo "Download info from internet"
+wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.93/bin/apache-tomcat-8.5.93.zip
