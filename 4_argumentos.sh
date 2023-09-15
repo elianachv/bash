3b80bbe9cca974bc9f4454a1e7cec090e802bc5b
Eliana Chavez <elianachavezv@gmail.com>
2023-09-15 12:35:15 +0000

    [DevOps][Class-8][Feature] Arguments script
    Creating a script wich receives 2 arguments then we show useful info
    like a phrase using the arguments, how many arguments were sent and the
    values of the arguments.
    
    Also permission for 3_tipoOperadores.sh file were changed to allow
    executions.

---
+++ 4_argumentos.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script to understand arguments
+# Author: Eliana Chávez - elianachavezv@gmail.com
+
+courseName=$1
+courseSchedule=$2
+
+echo "The name of course is $courseName and its schedule is $courseSchedule"
+echo "Number of params sent: $#"
+echo "Params sent $*"
