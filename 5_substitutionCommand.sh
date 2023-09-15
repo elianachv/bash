bc0ba1249be6d3a5e2efd695946d2dabb5e14cf4
Eliana Chavez <elianachavezv@gmail.com>
2023-09-15 12:50:16 +0000

    [DevOps][Class-9][Feature]: Executing commands within a script
    Create a script in wich the output of commands pwd and uname -a are kept
    into variables and then printed.

---
+++ 5_substitutionCommand.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script to check excute commands within a script and keep the output in a variable so then we can use
+# Author: Eliana Chavez - elianachavezv@gmail.com
+
+actualPath=`pwd`
+kernelInfo=$(uname -a)
+
+echo "Actual path: $actualPath"
+echo "Kernel info: $kernelInfo"
+
