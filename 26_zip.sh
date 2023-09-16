0ecfcb11c1ee7b4ca3763dbf4b10bb4ed63debcb
Eliana Chavez <elianachavezv@gmail.com>
2023-09-16 05:14:19 +0000

    [DevOps][Class-36][Feature]: Packing files with zip

---
+++ 26_zip.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Script to understand how to pack a script and assign a password
+# Author Eliana Chavez
+
+echo "Pack all the scripts in a zip file and secure the folder"
+zip -e shellCourse.zip *.sh
+
